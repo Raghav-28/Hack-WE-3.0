4dd22980d3999e3d939c541c399dba9108e7929a
Raghav-28 <kraghavgupta@gmail.com>
2025-01-27 14:31:35 +0530

    Updated Team

--- src/Componets/Team.jsx
+++ src/Componets/Team.jsx
@@ -9,11 +9,11 @@ import Deepak from "../assets/Team/Deepak.jpg";
 
 // Team data
 const teamMembers = [
-    { name: "Deepak", role: "Chief Organizer", photo:Deepak , Logo: Logo ,linkedin: "https://www.linkedin.com/in/kartik-chaudhari-0b1b3b1b7" },
-    { name: "Ishika", role: " Chief Organizer", photo:Ishika , Logo: Logo ,linkedin: "https://www.linkedin.com/in/kartik-chaudhari-0b1b3b1b7" },
-    { name: "Raghav Gupta", role: "Event Head", photo: Raghav , Logo: Logo ,linkedin: "https://www.linkedin.com/in/kartik-chaudhari-0b1b3b1b7" },
-    { name: "Inder Sharma", role: "Event Head", photo:Inder , Logo: Logo ,linkedin: "https://www.linkedin.com/in/kartik-chaudhari-0b1b3b1b7" },
-    { name: "Kunal Vashisht", role: " Coordinator", photo:kunal_dev , Logo: Logo ,linkedin: "https://www.linkedin.com/in/kartik-chaudhari-0b1b3b1b7" },
+    { name: "Deepak", role: "Chief Organizer", photo:Deepak , Logo: Logo ,linkedin: "https://tinyurl.com/34xpzeaf" },
+    { name: "Ishika", role: " Chief Organizer", photo:Ishika , Logo: Logo ,linkedin: "https://www.linkedin.com/in/ishigarg767?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+    { name: "Raghav Gupta", role: "Event Head", photo: Raghav , Logo: Logo ,linkedin: "https://www.linkedin.com/in/raghav-gupta-623764267/" },
+    { name: "Inder Sharma", role: "Event Head", photo:Inder , Logo: Logo ,linkedin: "https://www.linkedin.com/in/inder-sharma?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+    { name: "Kunal Vashisht", role: " Coordinator", photo:kunal_dev , Logo: Logo ,linkedin: "https://www.linkedin.com/in/kunal-vashisht-352nk?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
     
 ];
 
@@ -27,7 +27,7 @@ const Team = () => {
                         <img src={member.photo} alt={member.name} className="team-photo" />
                         <h3 className="team-name text-black font-[Ghr4]">{member.name}</h3>
                         <p className="team-role">{member.role}</p>
-                        {/* {member.linkedin && (
+                        {member.linkedin && (
                             <a
                                 href={member.linkedin}
                                 target="_blank"
@@ -36,7 +36,7 @@ const Team = () => {
                             >
                                 <img src={Logo} alt="" className="w-6 items-center LinkedIn"  />
                             </a>
-                        )} */}
+                        )}
                     </div>
                 ))}
             </div>
